4946145c56dc20d92ccb67aa860051cb371bb011
H3rmt <stemmer.enrico@gmail.com>
2023-06-26 02:27:00 +0200

    removed old

--- src/main.cpp
+++ src/main.cpp
@@ -8,33 +8,33 @@
 #define MICPIN A0
 #define AVGPIN A1
 
-#define LEDPIN 4
-#define LEDCOUNT 34
-#define BUTTONPIN 3
+// #define LEDPIN 4
+// #define LEDCOUNT 34
+// #define BUTTONPIN 3
 
-#define SUBLEDPIN 3
+#define SUBLEDPIN 2
 #define SUBLEDCOUNT 59
-#define SUBBUTTONPIN 9
+// #define SUBBUTTONPIN 9
 
-#define MIDLEDPIN1 2
-#define MIDLEDPIN2 4
+#define MIDLEDPIN1 3
+#define MIDLEDPIN2 6
 #define MIDLEDCOUNT 27
-#define MIDBUTTONPIN 10
+// #define MIDBUTTONPIN 10
 
 #define SIDELEDPIN1 5
 #define SIDELEDPIN2 6
 #define SIDELEDCOUNT 31
-#define SIDEBUTTONPIN 8
+// #define SIDEBUTTONPIN 8
 
 micinput input = micinput(MICPIN, AVGPIN, -140, 20, 30);
 strip<SUBLEDPIN, SUBLEDCOUNT> sub = strip<SUBLEDPIN, SUBLEDCOUNT>(false, 300, 1.5);
 strip<MIDLEDPIN1, MIDLEDCOUNT> mid1 = strip<MIDLEDPIN1, MIDLEDCOUNT>(true, 10, 1.5);
 strip<MIDLEDPIN2, MIDLEDCOUNT> mid2 = strip<MIDLEDPIN2, MIDLEDCOUNT>(false, 10, 1.5);
-strip<SIDELEDPIN1, SIDELEDCOUNT> side1 = strip<SIDELEDPIN1, SIDELEDCOUNT>(false, 10, 1.5);
+// strip<SIDELEDPIN1, SIDELEDCOUNT> side1 = strip<SIDELEDPIN1, SIDELEDCOUNT>(false, 10, 1.5);
 
-int subState = 6;
-int midState = 3;
-int sideState = 1;
+int subState = 7;
+int midState = 6;
+// int sideState = 1;
 
 long lastExec = 0;
 
@@ -70,7 +70,7 @@ void setup()
 	sub.init();
 	mid1.init();
 	mid2.init();
-	side1.init();
+	// side1.init();
 
 	// sub.Test2();
 	// delay(400);
@@ -84,9 +84,9 @@ void setup()
 	// sub.Test();
 	// delay(400);
 
-	pinMode(SUBBUTTONPIN, INPUT);
-	pinMode(MIDBUTTONPIN, INPUT);
-	pinMode(SIDEBUTTONPIN, INPUT);
+	// pinMode(SUBBUTTONPIN, INPUT);
+	// pinMode(MIDBUTTONPIN, INPUT);
+	// pinMode(SIDEBUTTONPIN, INPUT);
 }
 
 void loop()
@@ -97,42 +97,43 @@ void loop()
 	lastExec = currentMillis;
 
 	input.read();
+	Serial.println(input.getLvl());
 
-	if (digitalRead(SUBBUTTONPIN))
-	{
-		subState++;
-		// Serial.println(subState);
-		while (digitalRead(SUBBUTTONPIN))
-		{
-		}
-	}
+	// if (digitalRead(SUBBUTTONPIN))
+	// {
+	// 	subState++;
+	// 	// Serial.println(subState);
+	// 	while (digitalRead(SUBBUTTONPIN))
+	// 	{
+	// 	}
+	// }
 
-	if (digitalRead(MIDBUTTONPIN))
-	{
-		midState++;
-		// Serial.println(midState);
-		while (digitalRead(MIDBUTTONPIN))
-		{
-		}
-	}
+	// if (digitalRead(MIDBUTTONPIN))
+	// {
+	// 	midState++;
+	// 	// Serial.println(midState);
+	// 	while (digitalRead(MIDBUTTONPIN))
+	// 	{
+	// 	}
+	// }
 
-	if (digitalRead(SIDEBUTTONPIN))
-	{
-		sideState++;
-		// Serial.println(sideState);
-		while (digitalRead(SIDEBUTTONPIN))
-		{
-		}
-	}
+	// if (digitalRead(SIDEBUTTONPIN))
+	// {
+	// 	sideState++;
+	// 	// Serial.println(sideState);
+	// 	while (digitalRead(SIDEBUTTONPIN))
+	// 	{
+	// 	}
+	// }
 
-	if (subState >= 7)
-		subState = 0;
+	// if (subState >= 7)
+	// 	subState = 0;
 
-	if (midState >= 7)
-		midState = 0;
+	// if (midState >= 7)
+	// 	midState = 0;
 
-	if (sideState >= 7)
-		sideState = 0;
+	// if (sideState >= 7)
+	// 	sideState = 0;
 	// sub.Test();
 
 	// sub.Normal(input.getLvl(), input.getAvg(), true, true);
@@ -144,7 +145,7 @@ void loop()
 
 	// return;
 
-	subState = 7;
+	// subState = 7;
 
 	switch (subState)
 	{
@@ -176,7 +177,7 @@ void loop()
 
 	// return;
 
-	midState = 6;
+	// midState = 6;
 
 	switch (midState)
 	{
@@ -206,7 +207,6 @@ void loop()
 		break;
 	case 6:
 		mid1.Circle(input.getLvl(), input.getAvg(), true, 2, 0.08, 2, 2.0);
-		// mid2.Circle(input.getLvl(), input.getAvg(), true, 2, 0.08, 2, 2.0);
 		mid2.CentreOut(input.getLvl(), input.getAvg());
 		break;
 	case 7:
@@ -219,33 +219,33 @@ void loop()
 	checkCycles();
 	return;
 
-	sideState = 6;
+	// sideState = 6;
 
-	switch (sideState)
-	{
-	case 0:
-		side1.Clear();
-		break;
-	case 1:
-		side1.Normal(input.getLvl(), input.getAvg(), false);
-		break;
-	case 2:
-		side1.CentreOut(input.getLvl(), input.getAvg(), false);
-		break;
-	case 3:
-		side1.Normal(input.getLvl(), input.getAvg());
-		break;
-	case 4:
-		side1.CentreOut(input.getLvl(), input.getAvg());
-		break;
-	case 5:
-		side1.FallingStar(input.getLvl(), input.getAvg(), true, 3);
-		break;
-	case 6:
-		side1.Circle(input.getLvl(), input.getAvg(), true, 7, 0.05, 2, 2.5);
-		break;
-	}
+	// switch (sideState)
+	// {
+	// case 0:
+	// 	side1.Clear();
+	// 	break;
+	// case 1:
+	// 	side1.Normal(input.getLvl(), input.getAvg(), false);
+	// 	break;
+	// case 2:
+	// 	side1.CentreOut(input.getLvl(), input.getAvg(), false);
+	// 	break;
+	// case 3:
+	// 	side1.Normal(input.getLvl(), input.getAvg());
+	// 	break;
+	// case 4:
+	// 	side1.CentreOut(input.getLvl(), input.getAvg());
+	// 	break;
+	// case 5:
+	// 	side1.FallingStar(input.getLvl(), input.getAvg(), true, 3);
+	// 	break;
+	// case 6:
+	// 	side1.Circle(input.getLvl(), input.getAvg(), true, 7, 0.05, 2, 2.5);
+	// 	break;
+	// }
 
-	show();
-	checkCycles();
+	// show();
+	// checkCycles();
 }
