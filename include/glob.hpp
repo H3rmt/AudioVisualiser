70623b7f6b3513cc0479c352f3dbbb25efaa9638
H3rmt <stemmer.enrico@gmail.com>
2023-07-23 21:25:23 +0200

    added global file with #define's

---
+++ include/glob.hpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <FastLED.h>
+// #include <IRremote.hpp>
+
+#include "strip.hpp"
+#include "micInput.hpp"
+
+#define MICPIN A0
+#define AVGPIN A1
+
+#define REMOTEPIN 4
+
+#define SUBLEDPIN 2
+#define SUBLEDCOUNT 59
+
+#define MIDLEDPIN1 3
+#define MIDLEDPIN2 6
+#define MIDLEDCOUNT 27
+
+#define debugg
+
+
+
+// UpdatesPerSecond
+#define UPS 80
+
+#ifdef debug
+// SecondsUntilOff
+#define SUO 1
+#else
+#define SUO 4
+#endif
+
+
+// #define SIDELEDPIN1 5
+// #define SIDELEDPIN2 6
+// #define SIDELEDCOUNT 31
+
+
+#define chechTime1 long f = millis();
+#define chechTime2                \
+	Serial.print("time millis:"); \
+	Serial.println(millis() - f);
+
+#ifdef debug
+#define print(d) Serial.print(d);
+#define println(d) Serial.println(d);
+#else
+#define print(d)
+#define println(d)
+#endif
